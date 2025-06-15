5cd96d74627f36e0806ac5422d18662600cef82f
Ariel Ardiansyah <arielardiansyah050316@gmail.com>
2025-06-15 23:12:52 +0700

    Delete TugasKelasPbo directory

--- TugasKelasPbo/src2/Herbivora/Karakter.java
+++
@@ -1,7 +0,0 @@
-package Herbivora;
-
-// Interface berisi method wajib untuk class yang mengimplementasinya
-public interface Karakter {
-    void Musuh(String namaMusuh, String senjataMusuh); // Method untuk cerita musuh
-    void Pahlawan();                                   // Method untuk cerita pahlawan
-}
